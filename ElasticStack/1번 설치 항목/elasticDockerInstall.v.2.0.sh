dee502799d486dcbc1a14a75aa6bcf239a8b4def
cucuridas <3310223@naver.com>
2022-03-24 21:17:45 +0900

    Update Elasticsearch

--- ElasticStack/1번 설치 항목/elasticDockerInstall.v.2.0.sh
+++ ElasticStack/1번 설치 항목/elasticDockerInstall.v.2.0.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo "Elastic Search Docker Cotainer 설치스크립트 입니다 설치하고자 하는 version을 입력해주세요 ex) 7.2.0 "
+echo "Elastic Search Docker Cotainer 설치스크립트 입니다 설치하고자 하는 version을 입력해주세요 ex) 7.3.0 "
 read version
 
 echo "입력 받은 vesion으로 설치를 진행할게요~"
-#Elastic search, Kibana 이미지 받기 version 7.2.0
+#Elastic search, Kibana 이미지 받기 version 7.3.0
 docker pull docker.elastic.co/elasticsearch/elasticsearch:$version && docker pull docker.elastic.co/kibana/kibana:$version
 if [ $? -eq 0 ]  
 then
@@ -38,3 +38,33 @@ else
     echo "log: >>>>>>>>>> Failed"
     docker rmi -f docker.elastic.co/elasticsearch/elasticsearch:$version && docker rmi -f docker.elastic.co/kibana/kibana:$version
 fi    
+
+
+
+echo "Analyzer 설치를 진행하시겠어요? Y 또는 N으로 입력 바랍니다"
+read answer 
+
+if [[ $answer = "y" || $answer = "Y" ]]
+    then 
+        docker exec -it $esname bash bin/elasticsearch-plugin install analysis-nori
+        if [ $? -eq 0 ] 
+        then
+            echo "nori plugin Success"
+            echo "Plugin 반영을 위해 Elastic Search를 재부팅 할게요"
+            docker stop $esname && docker start $esname
+            echo "ElasticSearch 서비스가 실행 되기까지 기다려주세요!"
+        else
+            echo "log: >>>>>>>>>> Falid install analysis-nori, ElasticSearch실행 여부 확인필요"
+
+        fi
+    elif [[ $answer = "n" || $answer = "N" ]]
+        then
+            echo "OK"
+    else
+        echo " '$answer'란 값은 처리할 수 없는 값입니다"        
+    fi
+
+
+
+
+
